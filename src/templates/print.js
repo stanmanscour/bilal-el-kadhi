b8add8f0f98fe07567d13ede3c99338c7cf55c61
Stanislas Manscour <stan.manscour@gmail.com>
2020-03-05 22:54:27 +0100

    remove comments

--- src/templates/print.js
+++ src/templates/print.js
@@ -3,12 +3,6 @@ import Layout from "../components/Layout";
 import { graphql } from "gatsby";
 import PrintWrapper from "../styles/pages/print";
 
-// const print = {
-//   name: "Gucci",
-//   url:
-//     "https://format-com-cld-res.cloudinary.com/image/private/s--AgKG7X9L--/c_limit,g_center,h_65535,w_700/fl_keep_iptc.progressive,q_95/v1/11d7943ed5789a3cdffd1f4cc1e6b222/Stocksy_txp65fbad21uJ5200_Medium_1959815.jpg?700"
-// };
-
 export const query = graphql`
   query($slug: String!) {
     contentfulImage(slug: { eq: $slug }) {
@@ -27,7 +21,6 @@ const Print = props => {
   return (
     <Layout isShy={true} canGoBack={true} headerContentTitle={print.title}>
       <PrintWrapper>
-        {/* <PrintWrapper.Title>title</PrintWrapper.Title> */}
         <img src={print.media.file.url} />
       </PrintWrapper>
     </Layout>
