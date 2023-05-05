c390d52858e4aef5f8fa26af3c357918e3fa8844
Julie <julie.demendonca17@gmail.com>
2023-05-05 22:27:50 +0200

    :fire: Pages - Delete old pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -52,45 +52,9 @@ const createAllFilmPage = async (graphql, createPage) => {
   });
 };
 
-// const createAllContentPage = async (graphql, createPage, data) => {
-//   const { templatePath, contentfulName, beginningPath } = data;
-//   const template = path.resolve(templatePath);
-//   const response = await graphql(`
-//     query {
-//       ${contentfulName} {
-//         edges {
-//           node {
-//             films {
-//               slug
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `);
-
-//   const films = response.data[contentfulName].edges[0].node.films;
-//   films.forEach((film) => {
-//     createPage({
-//       component: template,
-//       path: `/${beginningPath}/${film.slug}`,
-//       context: {
-//         slug: film.slug,
-//       },
-//     });
-//   });
-// };
-
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
   await createAllPrintPage(graphql, createPage);
-
   await createAllFilmPage(graphql, createPage);
-
-  // await createAllContentPage(graphql, createPage, {
-  //   beginningPath: 'films',
-  //   templatePath: './src/templates/film.js',
-  //   contentfulName: 'allContentfulFilmsPage',
-  // });
 };
