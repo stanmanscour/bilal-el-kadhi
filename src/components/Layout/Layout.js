b9476fa03d1db2adf3d0a50c1c9bd02d78c31177
Julie <julie.demendonca17@gmail.com>
2023-09-15 20:31:18 +0200

    :lipstick: Layout - Remove useless import

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "../../styles/index.scss"
-import Header from "../Header"
-import Footer from "../Footer"
+// import Header from "../Header"
+// import Footer from "../Footer"
 
 import LayoutWrapper from "./elements"
 
