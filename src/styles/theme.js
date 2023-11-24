cd7d5e338b7107bcb6aec52500c8f77ac8e575fd
Julie De Mendonça <julie.demendonca@mmibordeaux.com>
2023-11-24 09:53:24 +0100

    :lipstick: Font - Udpate

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,18 +1,22 @@
-const THEME = {};
+const THEME = {}
 
-THEME.primaryFontFamily = "'franklin-gothic-atf', sans-serif";
-THEME.secondaryFontFamily = "Rubik";
+// THEME.primaryFontFamily = "'franklin-gothic-atf', sans-serif";
+THEME.primaryFontFamily = "'times new roman', serif"
+// THEME.secondaryFontFamily = "Rubik";
+THEME.secondaryFontFamily = "'times new roman', serif"
 
-THEME.MobileMax = "screen and (max-width: 768px)";
-THEME.TabletMin = "screen and (min-width: 769px)";
-THEME.DesktopMin = "screen and (min-width: 1025px)";
+THEME.MobileMax = 'screen and (max-width: 768px)'
+THEME.TabletMin = 'screen and (min-width: 769px)'
+THEME.DesktopMin = 'screen and (min-width: 1025px)'
 
-THEME.thinFontWeight = 300;
-THEME.mediumFontWeight = 500;
-THEME.boldFontWeight = 600;
+THEME.thinFontWeight = 300
+// THEME.mediumFontWeight = 500
+THEME.mediumFontWeight = 300
+// THEME.boldFontWeight = 600
+THEME.boldFontWeight = 300
 
 THEME.colors = {
-  grey: "#9b9b9b"
-};
+  grey: '#9b9b9b'
+}
 
-export default THEME;
+export default THEME
